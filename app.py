f854343ff55a1d091f693897763309201da0fc5b
amerchant918 <amerchant918@gmail.com>
2020-11-05 21:00:51 -0600

    Testing interactive element bmi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/templates', methods=['GET', 'POST'])
 def index():
     bmi = ''
     if request.method == 'POST' and 'weight' in request.form:

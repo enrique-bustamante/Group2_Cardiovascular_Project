631e39b1bd286b5eff08663a457c86f2b0d1bfa8
amerchant918 <amerchant918@gmail.com>
2020-11-08 11:23:27 -0600

    reversing the style.css to old version to correct the webpage

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/templates', methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/index', methods=['POST'])
 def index():
     bmi = ''
     if request.method == 'POST' and 'weight' in request.form:
@@ -11,7 +15,8 @@ def index():
         bmi = calc_bmi(weight, height)
     return render_template("index.html",
 	                        bmi=bmi)
-
+                            
+@app.route('/calc_bmi', methods=['POST'])
 def calc_bmi(weight, height):
     return round((weight / ((height / 100) ** 2)*703), 2)
 

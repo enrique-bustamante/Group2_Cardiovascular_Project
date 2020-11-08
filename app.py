da7e83ad7aedff36c4325b081cd916c13c59aa31
amerchant918 <amerchant918@gmail.com>
2020-11-08 13:44:50 -0600

    updating with Saras index html

--- app.py
+++ app.py
@@ -2,23 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/index', methods=['POST'])
-def index():
-    bmi = ''
-    if request.method == 'POST' and 'weight' in request.form:
-        weight = float(request.form.get('weight'))
-        height = float(request.form.get('height'))
-        bmi = calc_bmi(weight, height)
-    return render_template("index.html",
-	                        bmi=bmi)
-                            
-@app.route('/calc_bmi', methods=['POST'])
-def calc_bmi(weight, height):
-    return round((weight / ((height / 100) ** 2)*703), 2)
+@app.route('/',methods=['POST','GET'])
+def calculate():
+    bmi=''
+    if request.method=='POST' and 'weight' in request.form and 'height' in request.form:
+        Weight=float(request.form.get('weight'))
+        Height=float(request.form.get('height'))
+        bmi=round(((Weight/(Height**2))*703),2)
+    return render_template("index.html",bmi=bmi)
 
 if __name__ == '__main__':
     app.run()

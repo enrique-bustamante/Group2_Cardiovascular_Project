4bee8458ed4ed5f07259a5f1cb0c39231d4948a3
amerchant918 <amerchant918@gmail.com>
2020-11-12 19:56:34 -0600

    Modifying second interactive element to create an open array

--- app.py
+++ app.py
@@ -15,8 +15,25 @@ def calculate():
     if request.method=='POST' and 'weight' in request.form and 'height' in request.form:
         Weight=float(request.form.get('weight'))
         Height=float(request.form.get('height'))
-        bmi=round((Weight/(Height**2))*703)
+        bmi=round(((Weight/(Height**2))*703),2)
     return render_template("index2.html",bmi=bmi)
+
+@app.route('/machinelearning',methods=['POST'])
+def machinelearning():
+    answer=[]
+    if request.method=='POST' and 'age' in request.form and 'gender' in request.form and 'bmi' in request.form and 'ap_hi' in request.form and 'ap_lo' in request.form and 'chol' in request.form and 'gluc' in request.form:
+        Age=float(request.form.get('age'))
+        Gender=float(request.form.get('gender'))
+        BMI=float(request.form.get('bmi'))
+        Systolic=float(request.form.get('ap_hi'))
+        Diastolic=float(request.form.get('ap_lo'))
+        Cholesterol=float(request.form.get('chol'))
+        Glucose=float(request.form.get('gluc'))
+#Creating an Array 
+        #answer=Age+Gender+BMI+Systolic+Diastolic+Cholesterol+Glucose
+        answer.append([Age,Gender,BMI,Systolic,Diastolic,Cholesterol,Glucose])
+    print(answer)
+    return render_template("index2.html",answer=answer)
 
 if __name__ == '__main__':
     app.run()
